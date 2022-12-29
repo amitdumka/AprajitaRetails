150b87bba5708112469c1dd7d0e7bc78efb44b51
Amit Kumar <amitnarayansah@gmail.com>
2022-12-29 17:52:15 +0530

    added few table class

--- AprajitaRetails.Shared.Models/Models/Vouchers/Voucher.cs
+++ AprajitaRetails.Shared.Models/Models/Vouchers/Voucher.cs
@@ -1,39 +1,191 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using AprajitaRetails.Shared.Models.Models.Base;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace AprajitaRetails.Shared.Models.Models.Vouchers
 {
-    public class Voucher
+    //public class Voucher
+    //{
+    //    [Key]
+    //    public string VoucherNo { get; set; }
+    //    public string VoucherType { get; set;}
+    //    public DateTime OnDate { get; set; }
+    //    public string PaidTo { get; set; }
+    //    public decimal Amount { get; set; }
+    //    public string PayMode { get; set; }
+    //    public string PaymentDetails { get; set; }
+    //    public string Remarks { get; set; }
+    //    public string IssuedBy { get; set; }
+    //    public string PartyId { get; set; }
+
+    //}
+
+    //public class CashVoucher
+    //{
+    //    [Key]
+    //    public string CashVoucherNo { get; set; }
+    //    public string CategoryId { get; set; }
+    //    public string VoucherType { get; set; }
+    //    public DateTime OnDate { get; set; }
+    //    public string PaidTo { get; set; }
+    //    public decimal Amount { get; set; }
+    //    public string Remarks { get; set; }
+    //    public string IssuedBy { get; set; }
+    //}
+    public class Voucher : BaseST
     {
         [Key]
-        public string VoucherNo { get; set; }
-        public string VoucherType { get; set;}
+        public string VoucherNumber { get; set; }
+        public VoucherType VoucherType { get; set; }
         public DateTime OnDate { get; set; }
-        public string PaidTo { get; set; }
+        public string SlipNumber { get; set; }
+
+        public string PartyName { get; set; }
+
+        public string Particulars { get; set; }
+
         public decimal Amount { get; set; }
-        public string PayMode { get; set; }
+        public PaymentMode PaymentMode { get; set; }
         public string PaymentDetails { get; set; }
         public string Remarks { get; set; }
-        public string IssuedBy { get; set; }
-        public string PartyId { get; set; }
+        public string AccountId { get; set; }
+        public string EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
 
+        public string PartyId { get; set; }
+        public virtual Party Partys { get; set; }
     }
 
-    public class CashVoucher
+    [Table("V1_TranscationModes")]
+    public class TranscationMode
     {
         [Key]
-        public string CashVoucherNo { get; set; }
-        public string CategoryId { get; set; }
-        public string VoucherType { get; set; }
-        public DateTime OnDate { get; set; }
-        public string PaidTo { get; set; }
-        public decimal Amount { get; set; }
-        public string Remarks { get; set; }
-        public string IssuedBy { get; set; }
+        public string TranscationId { get; set; }
+        public string TranscationName { get; set; }
     }
 
+    public class CashVoucher : BaseST
+    {
+
+        [Key]
+        public string VoucherNumber { get; set; }
+        public VoucherType VoucherType { get; set; }
+        public DateTime OnDate { get; set; }
+
+
+        public string TranscationId { get; set; }
+
+        [ForeignKey("TranscationId")]
+        public virtual TranscationMode TranscationMode { get; set; }
+
+        public string SlipNumber { get; set; }
+        public string PartyName { get; set; }
+        public string Particulars { get; set; }
+        public decimal Amount { get; set; }
+        public string Remarks { get; set; }
+        public string EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public string PartyId { get; set; }
+        public virtual Party Partys { get; set; }
+
+    }
+    [Table("V1_Notes")]
+    public class Note : BaseST
+    {
+        [Key]
+        public string NoteNumber { get; set; }
+        public NotesType NotesType { get; set; }
+        public DateTime OnDate { get; set; }
+
+        public string PartyName { get; set; }
+        public bool WithGST { get; set; }
+        public decimal Amount { get; set; }
+        public decimal TaxRate { get; set; }
+        public decimal TaxAmount { get { return (Amount * (TaxRate / 100)); } }
+        public decimal NetAmount { get { return Amount + TaxAmount; } }
+        public string Reason { get; set; }
+        public string Remarks { get; set; }
+
+        public string PartyId { get; set; }
+        public virtual Party Party { get; set; }
+    }
+
+    public class LedgerGroup
+    {
+        [Key]
+        public string LedgerGroupId { get; set; }
+        public string GroupName { get; set; }
+        public LedgerCategory Category { get; set; }
+        public string Remark { get; set; }
+
+
+    }
+    [Table("V1_Parties")]
+    public class Party : BaseST
+    {
+        public string PartyId { get; set; }
+        public string PartyName { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public DateTime? ClosingDate { get; set; }
+        public decimal OpeningBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
+        public LedgerCategory Category { get; set; }
+        public string GSTIN { get; set; }
+        public string PANNo { get; set; }
+        public string Address { get; set; }
+        public string Remarks { get; set; }
+        public string LedgerGroupId { get; set; }
+    }
+    [Table("V1_LedgerMasters")]
+    public class LedgerMaster
+    {
+        [Key]
+        public string PartyId { get; set; }
+        public string PartyName { get; set; }
+        public DateTime OpeningDate { get; set; }
+    }
+
+    [Table("V1_PettyCashSheets")]
+    public class PettyCashSheet
+    {
+        public string Id { get; set; }
+        public DateTime OnDate { get; set; }
+        //Balance
+        public decimal OpeningBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
+
+        //Bank
+        public decimal BankDeposit { get; set; }
+        public decimal BankWithdrawal { get; set; }
+
+        //Sale
+        public decimal DailySale { get; set; }
+
+        public decimal TailoringSale { get; set; }
+        public decimal TailoringPayment { get; set; }
+
+        //Different Sale
+        public decimal ManualSale { get; set; }
+        public decimal CardSale { get; set; }
+        public decimal NonCashSale { get; set; }
+
+        public decimal CustomerDue { get; set; }
+        public string DueList { get; set; }
+        public decimal CustomerRecovery { get; set; }
+        public string RecoveryList { get; set; }
+
+        public string ReceiptsNaration { get; set; }
+        public decimal ReceiptsTotal { get; set; }
+
+        public string PaymentNaration { get; set; }
+        public decimal PaymentTotal { get; set; }
+
+    }
 
 
 }
