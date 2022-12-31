dd179338c892a570a6c9509590f70689641779b4
Amit Kumar <amitsha.2407@gmail.com>
2022-12-31 22:40:58 +0530

    dark mode added

---
+++ AprajitaRetails/Client/AdminLte/Class.cs
@@ -0,0 +1,29 @@
+﻿namespace AprajitaRetails.Client.AdminLte
+//{
+//	public class Class
+//	{
+//	}
+//}
+//namespace Blazor.AdminLte.Enumerations
+{
+	public enum NavBarColor
+	{
+		Primary,
+		Secondary,
+		Info,
+		Success,
+		Danger,
+		Indigo,
+		Purple,
+		Pink,
+		Navy,
+		Lightblue,
+		Teal,
+		Cyan,
+		GrayDark,
+		Gray,
+		Warning,
+		White,
+		Orange
+	}
+}
