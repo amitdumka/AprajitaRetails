2f2ba10da9b30190e5e82f2e99454c58565f9c95
Amit Kumar <amitsha.2407@gmail.com>
2023-01-02 21:20:48 +0530

    voucher edit

--- AprajitaRetails/Client/AdminLte/Class.cs
+++ AprajitaRetails/Client/AdminLte/Class.cs
@@ -1,4 +1,6 @@
-﻿namespace AprajitaRetails.Client.AdminLte
+﻿using Blazor.AdminLte;
+
+namespace AprajitaRetails.Client.AdminLte
 //{
 //	public class Class
 //	{
@@ -26,4 +28,68 @@
 		White,
 		Orange
 	}
+    public enum ButtonSize
+    {
+        [Style("")]
+        Normal,
+        [Style("xs")]
+        ExtraSmall,
+        [Style("sm")]
+        Small,
+        [Style("md")]
+        Medium,
+        [Style("lg")]
+        Large,
+        [Style("xl")]
+        ExtraLarge,
+        [Style("flat")]
+        Flat
+    }
+    //public enum Color
+    //{
+    //    [Style("")]
+    //    Default,
+    //    [Style("danger")]
+    //    Danger,
+    //    [Style("warning")]
+    //    Warning,
+    //    [Style("primary")]
+    //    Primary,
+    //    [Style("secondary")]
+    //    Secondary,
+    //    [Style("info")]
+    //    Info,
+    //    [Style("success")]
+    //    Success,
+    //    [Style("black")]
+    //    Black,
+    //    [Style("gray-dark")]
+    //    GrayDark,
+    //    [Style("gray")]
+    //    Gray,
+    //    [Style("light")]
+    //    Light,
+    //    [Style("indigo")]
+    //    Indigo,
+    //    [Style("navy")]
+    //    Navy,
+    //    [Style("purple")]
+    //    Purple,
+    //    [Style("fuchsia")]
+    //    Fuchsia,
+    //    [Style("pink")]
+    //    Pink,
+    //    [Style("maroon")]
+    //    Maroon,
+    //    [Style("orange")]
+    //    Orange,
+    //    [Style("lime")]
+    //    Lime,
+    //    [Style("teal")]
+    //    Teal,
+    //    [Style("olive")]
+    //    Olive,
+    //    [Style("white")]
+    //    White
+    //}
 }
