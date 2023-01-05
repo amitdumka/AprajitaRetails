d94976bf638394eedf513918bb431e3021a49c94
Amit Kumar <amitnarayansah@gmail.com>
2023-01-05 23:59:41 +0530

    Custom Login workling

--- AprajitaRetails/Client/Program.cs
+++ AprajitaRetails/Client/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("AprajitaRetails.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("AprajitaRetails.ServerAPI.Auth", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AprajitaRetails.ServerAPI"));
