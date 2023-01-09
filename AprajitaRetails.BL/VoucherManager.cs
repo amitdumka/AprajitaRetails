daafb0c4c9cc0d855c0e2415095cba63f372b494
Amit Kumar <amitsha.2407@gmail.com>
2023-01-10 00:54:18 +0530

    bug fixed voucher and attendance is adding

--- AprajitaRetails.BL/VoucherManager.cs
+++ AprajitaRetails.BL/VoucherManager.cs
@@ -1,6 +1,5 @@
 ﻿namespace AprajitaRetails.BL.Vouchers;
 
-
 public class VoucherManager
 {
     public static string GenerateVoucherNumber(VoucherType type, string storeId, DateTime onDate, int count)
@@ -12,35 +11,39 @@ public class VoucherManager
             case VoucherType.Payment:
                 typ = "PYM";
                 break;
+
             case VoucherType.Receipt:
                 typ = "RCT";
                 break;
+
             case VoucherType.Contra:
                 break;
+
             case VoucherType.DebitNote:
                 break;
+
             case VoucherType.CreditNote:
                 break;
+
             case VoucherType.JV:
                 break;
+
             case VoucherType.Expense:
                 typ = "EXP";
                 break;
+
             case VoucherType.CashReceipt:
                 typ = "PCT";
                 break;
+
             case VoucherType.CashPayment:
                 typ = "CPT";
                 break;
+
             default:
                 typ = "PYM";
-                    break;
+                break;
         }
         return $"{storeId}/${typ}/{onDate.Year}/{onDate.Month}/{onDate.Day}/{count}";
     }
-
-
-
 }
-
-
