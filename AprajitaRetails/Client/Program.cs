97ce23fdaa933e107429391addbdc9b5086522b4
Amit Kumar <amitnarayansah@gmail.com>
2023-01-11 18:47:34 +0530

    some bug select bug

--- AprajitaRetails/Client/Program.cs
+++ AprajitaRetails/Client/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -18,6 +19,7 @@ builder.Services.AddHttpClient("AprajitaRetails.ServerAPI.Auth", client => clien
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AprajitaRetails.ServerAPI"));
 builder.Services.AddAdminLte();
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped<IFilesManager, WasmFilesManager>();
 builder.Services.AddScoped<LocalStorageAccessor>();
 
