7503fbad1c5f6b9972974d8a87f9df1b5de551b3
Amit Kumar <amitsha.2407@gmail.com>
2023-01-31 15:10:38 +0530

    project created

---
+++ AprajitaRetails/ClientShared/AprajitaRetails.UILib/Shared/BasicViews/IndexView.razor.cs
@@ -0,0 +1,92 @@
+﻿using Syncfusion.Blazor.Grids;
+using System.Globalization;
+using System.Reflection;
+
+namespace AprajitaRetails.BasicViews
+{
+    public partial class IndexView
+    {
+        protected List<GridColumn> GridCols;
+        protected string[] GroupedColumn;
+
+        protected void InitView()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("hi-IN", false);
+            CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol = "₹";
+            if (string.IsNullOrEmpty(Setting.StoreCode))
+            {
+                Helper.Msg("Error", "Select store!, Kindly re-login", true);
+                return;
+            }
+
+
+        }
+
+        protected void GenerateColums(PropertyInfo[] infos, string idName)
+        {
+            GridCols = new List<GridColumn>();
+            foreach (var prop in infos)
+            {
+                if (prop.Name.EndsWith("Id")==false && prop.Name.EndsWith("ID") == false )
+                    //&& prop.Name != "EmployeeId" && prop.Name != "TransactionId" 
+                    //&& prop.Name != "TransactionMode" && prop.Name != "PartyId" && prop.Name != "StoreId")
+                {
+                    var v = new GridColumn()
+                    {
+                        AutoFit = true,
+
+                        Field = prop.Name,
+                        AllowSorting = true,
+                        IsPrimaryKey = prop.Name == idName ? true : false,
+                        AllowEditing = prop.CanWrite,
+                        HeaderText = prop.Name,
+                        HeaderTextAlign = Syncfusion.Blazor.Grids.TextAlign.Center
+                    };
+                    if (prop.GetType() == typeof(decimal))
+                    {
+                        if (prop.Name.Contains("Amount"))
+                            v.Format = "C2";
+                    }
+
+                    GridCols.Add(v);
+                }
+            }
+
+            var CommandsList = new List<GridCommandColumn>();
+            var edit = new GridCommandColumn()
+            {
+                ID = "edit",
+                Title = "Edit",
+                Type = CommandButtonType.None,
+                ButtonOption = new CommandButtonOptions() { IconCss = "e-icons e-edit", CssClass = "e-flat" }
+            };
+            var delete = new GridCommandColumn()
+            {
+                ID = "delete",
+                Title = "Delete",
+                Type = CommandButtonType.None,
+                ButtonOption = new CommandButtonOptions() { IconCss = "e-icons e-delete", CssClass = "e-flat" }
+            };
+            var info = new GridCommandColumn()
+            {
+                ID = "info",
+                Title = "Detail",
+                Type = CommandButtonType.None,
+                ButtonOption = new CommandButtonOptions() { IconCss = "e-icons e-update", CssClass = "e-flat" }
+            };
+            CommandsList.Add(info);
+            CommandsList.Add(edit);
+            CommandsList.Add(delete);
+            var cCol = new GridColumn()
+            {
+                HeaderText = "Actions",
+                AutoFit = true,
+                Commands = CommandsList
+
+            };
+            GridCols.Add(cCol);
+        }
+
+
+    }
+}
