52384947f5fcfac8628674c77301e86fe27d9bbf
Amit Kumar <amitnarayansah@gmail.com>
2023-02-03 23:18:20 +0530

    major changes

---
+++ AprajitaRetails/Server/Controllers/Reports/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AprajitaRetails.Server.Data;
+using AprajitaRetails.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace AprajitaRetails.Server.Controllers.Reports
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+
+        private readonly ARDBContext _context;
+        public DashboardController(ARDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("StoreManagerDashboard")]
+        public async Task<ActionResult<IEnumerable<SelectOption>>> GetLedgerGroups(string storeid)
+        {
+            if (_context.LedgerGroups == null)
+            {
+                return NotFound();
+            }
+            return await _context.LedgerGroups.Select(c => new SelectOption { ID = c.LedgerGroupId, Value = c.GroupName }).ToListAsync();
+        }
+    }
+}
+
---
+++ AprajitaRetails/Shared/ViewModels/Dashboards /DashboardVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace AprajitaRetails.Shared.ViewModels.Dashboards
+{
+	public class DashboardVM
+	{
+		public string StoreId { get; set; }
+
+		public decimal TodaySale { get; set; }
+        public decimal MonhtlySale { get; set; }
+        public decimal WeeklySale { get; set; }
+        public decimal YearlySale { get; set; }
+        public decimal QuatarlySale { get; set; }
+
+        public decimal YearlyPayments { get; set; }
+        public decimal YearlyReciepts { get; set; }
+        public decimal YearlyExpenses { get; set; }
+
+        public decimal MonthlyPayments { get; set; }
+        public decimal MonthlyReciepts { get; set; }
+        public decimal MonthlyExpenses { get; set; }
+
+        public decimal YearlySalaries { get; set; }
+        public decimal MonthlySalaries { get; set; }
+        public decimal QuartlyExpenses { get; set; }
+ 
+    }
+
+    public class PayrollInfo
+    {
+        public string StoreId { get; set; }
+        public List<EmpInfo> Emps { get; set; }
+ 
+    }
+
+    public class EmpInfo
+    {
+        public string Name { get; set; }
+        public AttUnit Today { get; set; }
+        public decimal Present { get; set; }
+        public decimal TotalSale { get; set; }
+
+    }
+
+}
+
