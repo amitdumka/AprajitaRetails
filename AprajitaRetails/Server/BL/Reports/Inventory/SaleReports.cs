761259cdad757d3a9c27f5ce23ddbd59a3f4139b
Amit Kumar <amitsha.2407@gmail.com>
2023-04-07 17:46:47 +0530

    Create SaleReports.cs

---
+++ AprajitaRetails/Server/BL/Reports/Inventory/SaleReports.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AprajitaRetails.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AprajitaRetails.Server.BL.Reports.Inventory
+{
+	public class SaleReports
+	{
+		public static void GenerateSaleReportForGST(ARDBContext db, int Month, int Year)
+		{
+
+			var saleData = db.SaleItems.Include(c => c.ProductSale).Where(c => c.ProductSale.OnDate.Year == Year && c.ProductSale.OnDate.Month == Month)
+				.Select(c => new {c.Barcode, c.BilledQty, c.FreeQty, c.BasicAmount, c.DiscountAmount, c.TaxAmount, c.Value,c.ProductSale.StoreId })
+				.ToList();
+
+		}
+	}
+}
+
