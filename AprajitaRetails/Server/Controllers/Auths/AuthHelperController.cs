8bd2841727a3d22e7a65108e510f46e372d0df83
Amit Kumar <amitsha.2407@gmail.com>
2023-09-28 09:10:03 +0530

    ok

--- AprajitaRetails/Server/Controllers/Auths/AuthHelperController.cs
+++ AprajitaRetails/Server/Controllers/Auths/AuthHelperController.cs
@@ -1,5 +1,7 @@
-﻿using AprajitaRetails.Server.Data;
+﻿using AprajitaRetails.Server.BL.Accounts;
+using AprajitaRetails.Server.Data;
 using AprajitaRetails.Server.Models;
+using AprajitaRetails.Shared.Models.Vouchers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -23,24 +25,68 @@ namespace AprajitaRetails.Server.Controllers.Auths
         }
 
 
+        [HttpGet("UpdateExpenses")]
+        public async Task<ActionResult<bool?>> GetTranferToxpenses(string PartyId)
+        {
+
+            var vouchers = _context.Vouchers.Where(c => c.PartyId == PartyId && c.VoucherType == VoucherType.Payment).ToList();
+
+            foreach (var item in vouchers)
+            {
+                Voucher nvoc = new Voucher
+                {
+                    AccountId = item.AccountId,
+                    Amount = item.Amount,
+                    EmployeeId = item.EmployeeId,
+                    EntryStatus = item.EntryStatus,
+                    IsReadOnly = true,
+                    MarkedDeleted = item.MarkedDeleted,
+                    OnDate = item.OnDate,
+                    Particulars = item.Particulars,
+                    PartyId = item.PartyId,
+                    PartyName = item.PartyName,
+                    PaymentDetails = item.PaymentDetails,
+                    PaymentMode = item.PaymentMode,
+                    Remarks = item.Remarks,
+                    StoreId = item.StoreId,
+                    UserId = "AutoAdmin",
+                    SlipNumber = item.SlipNumber,
+                    VoucherType = VoucherType.Expense,
+                };
+
+
+
+
+                nvoc.VoucherNumber = AccountHelper.VoucherNumberGenerator(_context, VoucherType.Expense, nvoc.StoreId, nvoc.OnDate);
+                _context.Vouchers.Add(nvoc);
+                _context.Vouchers.Remove(item);
+            }
+
+            bool x = _context.SaveChanges() > 0;
+            if (x) return Ok("Updated");
+            else return Problem("Error");
+
+        }
+
+
         [HttpGet("UpdateGroupId")]
         public async Task<ActionResult<bool?>> GetUpdateUser()
         {
-            var list=_authDb.Users.ToList();
+            var list = _authDb.Users.ToList();
             foreach (var item in list)
             {
                 item.Approved = true; item.StoreGroupId = "TAS";
-                item.AppClinetId = Guid.Parse( "a765c480-25c8-440b-9fc4-047e4a66834f");
+                item.AppClinetId = Guid.Parse("a765c480-25c8-440b-9fc4-047e4a66834f");
                 _authDb.Users.Update(item);
                 if (item.UserName == "AmitKumar") { item.UserType = UserType.SuperAdmin; item.Permission = RolePermission.Owner; }
 
                 else
                 {
                     item.UserType = UserType.PowerUser; item.Permission = RolePermission.StoreManager;
-                    
+
                 }
             }
-            return _authDb.SaveChanges()>0;
+            return _authDb.SaveChanges() > 0;
         }
 
         [HttpGet("UpdateBankAccount")]
@@ -51,7 +97,7 @@ namespace AprajitaRetails.Server.Controllers.Auths
             {
                 item.AppClientId = Guid.Parse("a765c480-25c8-440b-9fc4-047e4a66834f");
                 item.StoreGroupId = "TAS";
-               
+
             }
             return _context.SaveChanges() > 0;
         }
