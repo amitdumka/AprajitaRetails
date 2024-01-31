529df07f9ded01df5691caaa24730fc3c43ed203
Amit Kumar <amitsha.2407@gmail.com>
2024-01-31 20:22:09 +0530

    basic mode change

--- AprajitaRetails.Services.InfoManager/Program.cs
+++ AprajitaRetails.Services.InfoManager/Program.cs
@@ -14,16 +14,23 @@ using Microsoft.Extensions.Hosting;
 //var host = builder.Build();
 //host.Run();
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
 
 builder.Services.AddWindowsService();
+builder.Services.AddHostedService<Worker>();
+
 builder.Services.AddHostedService<ServiceA>();
+builder.Services.AddHostedService<TimedHostedService>();
+
+builder.Services.AddHostedService<TimedHostedService2>();
+builder.Services.AddHostedService<ConsumeScopedServiceHostedService>();
+builder.Services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
 
 var app = builder.Build();
 
-app.MapRazorPages();
+//app.MapRazorPages();
 
 app.Run();
 
--- AprajitaRetails.Services.InfoManager/Worker.cs
+++ AprajitaRetails.Services.InfoManager/Worker.cs
@@ -17,7 +17,7 @@ namespace AprajitaRetails.Services.InfoManager
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(100000, stoppingToken);
             }
         }
     }
@@ -40,13 +40,172 @@ namespace AprajitaRetails.Services.InfoManager
             {
                 Logger.LogInformation("ServiceA is doing background work.");
 
-                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+                await Task.Delay(TimeSpan.FromSeconds(120), stoppingToken);
             }
 
             Logger.LogInformation("ServiceA has stopped.");
         }
 
     }
+
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer? _timer = null;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("TS 001  Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(1800));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation(
+                "TS 001 Service is working. Count: {Count}", count);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("TS 001 Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+    internal interface IScopedProcessingService
+    {
+        Task DoWork(CancellationToken stoppingToken);
+    }
+
+    internal class ScopedProcessingService : IScopedProcessingService
+    {
+        private int executionCount = 0;
+        private readonly ILogger _logger;
+
+        public ScopedProcessingService(ILogger<ScopedProcessingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DoWork(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                executionCount++;
+
+                _logger.LogInformation(
+                    "Scoped Processing Service is working. Count: {Count}", executionCount);
+
+                await Task.Delay(100000, stoppingToken);
+            }
+        }
+    }
+    public class ConsumeScopedServiceHostedService : BackgroundService
+    {
+        private readonly ILogger<ConsumeScopedServiceHostedService> _logger;
+
+        public ConsumeScopedServiceHostedService(IServiceProvider services,
+            ILogger<ConsumeScopedServiceHostedService> logger)
+        {
+            Services = services;
+            _logger = logger;
+        }
+
+        public IServiceProvider Services { get; }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service running.");
+
+            await DoWork(stoppingToken);
+        }
+
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service is working.");
+
+            using (var scope = Services.CreateScope())
+            {
+                var scopedProcessingService =
+                    scope.ServiceProvider
+                        .GetRequiredService<IScopedProcessingService>();
+
+                await scopedProcessingService.DoWork(stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service is stopping.");
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+
+    public class TimedHostedService2 : BackgroundService
+    {
+        private readonly ILogger<TimedHostedService2> _logger;
+        private int _executionCount;
+
+        public TimedHostedService2(ILogger<TimedHostedService2> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("TS 002 Service running.");
+
+            // When the timer should have no due-time, then do the work once now.
+            DoWork();
+
+            using PeriodicTimer timer = new(TimeSpan.FromSeconds(180));
+
+            try
+            {
+                while (await timer.WaitForNextTickAsync(stoppingToken))
+                {
+                    DoWork();
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                _logger.LogInformation("Ts 002 Service is stopping.");
+            }
+        }
+
+        // Could also be a async method, that can be awaited in ExecuteAsync above
+        private void DoWork()
+        {
+            int count = Interlocked.Increment(ref _executionCount);
+
+            _logger.LogInformation("TS 002 Service is working. Count: {Count}", count);
+        }
+    }
+
+
 }
 
 //https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service
