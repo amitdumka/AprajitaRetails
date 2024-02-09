6290d3e2206b6761b1640da4f9317263d208c134
Amit Kumar <amitsha.2407@gmail.com>
2024-02-09 18:11:30 +0530

    create pdf . need clientside  code

--- AprajitaRetails/Server/BL/Accounts/Accounts.cs
+++ AprajitaRetails/Server/BL/Accounts/Accounts.cs
@@ -2,7 +2,18 @@
 using AprajitaRetails.Server.Data;
 using AprajitaRetails.Shared.Models.Stores;
 using AprajitaRetails.Shared.ViewModels;
-
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+using Microsoft.EntityFrameworkCore;
+using AprajitaRetails.Server.Data;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using Syncfusion.Drawing;
+using AutoMapper.Configuration.Conventions;
+using Syncfusion.Blazor.Schedule.Internal;
+using System.Linq;
 namespace AprajitaRetails.Server.BL.Accounts
 {
 
@@ -40,12 +51,118 @@ namespace AprajitaRetails.Server.BL.Accounts
             return  data;
         }
 
-        public static void GeneratePdfLedgerReport(string PartyName, string StoreName, string StoreAddress, string filename)
+        
+        public static void GenerateExcelLedgerReport(string PartyName, string StoreName, string StoreAddress, string filename)
         {
 
         }
-        public static void GenerateExcelLedgerReport(string PartyName, string StoreName, string StoreAddress, string filename)
+        public static MemoryStream GeneratePdfLedgerReport(IEnumerable<LedgerDetail>?data , string PartyName, string StoreName, string StoreAddress, string filename)
         {
+
+           
+
+            using (PdfDocument pdfDocument = new PdfDocument())
+            {
+                int paragraphAfterSpacing = 8;
+                int cellMargin = 8;
+                //Add page to the PDF document.
+                PdfPage page = pdfDocument.Pages.Add();
+                //Create a new font.
+                PdfStandardFont font = new PdfStandardFont(PdfFontFamily.TimesRoman, 16);
+                //Create a text element to draw a text in PDF page.
+                PdfTextElement title = new PdfTextElement($"Ledger Book: {PartyName}", font, PdfBrushes.DarkRed);
+
+                PdfLayoutResult result = title.Draw(page, new PointF(0, 0));
+                PdfStandardFont contentFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 12);
+                PdfTextElement content = new PdfTextElement("Ledger Book  Report is generated for Each Store/Group", contentFont, PdfBrushes.Black);
+                PdfLayoutFormat format = new PdfLayoutFormat();
+                format.Layout = PdfLayoutType.Paginate;
+                //Draw a text to the PDF document.
+                result = content.Draw(page, new RectangleF(0, result.Bounds.Bottom + paragraphAfterSpacing, page.GetClientSize().Width, page.GetClientSize().Height), format);
+
+                
+                    PdfTextElement stitle = new PdfTextElement($"Store: {StoreName}, \n{StoreAddress}", font, PdfBrushes.DarkRed);
+                    result = stitle.Draw(page, new PointF(0, 0));
+
+                    PdfGrid pdfGrid = new PdfGrid();
+                    pdfGrid.Style.CellPadding.Left = cellMargin;
+                    pdfGrid.Style.CellPadding.Right = cellMargin;
+
+                    //Assign data source.
+                    pdfGrid.DataSource = data;
+                
+
+                    //Applying built-in style to the PDF grid.
+                    pdfGrid.ApplyBuiltinStyle(PdfGridBuiltinStyle.GridTable4Accent1);
+                    pdfGrid.Style.Font = contentFont;
+                    //Draw PDF grid into the PDF page.
+                    pdfGrid.Draw(page, new PointF(0, result.Bounds.Bottom + paragraphAfterSpacing));
+                    PdfTextElement footer = new PdfTextElement($"Ledger Summary  : Total In: {data.Sum(c=>c.InAmount)} \t Out:  {data.Sum(c => c.OutAmount)}\t Balance: {data.Sum(c => c.InAmount-c.OutAmount)}", font, PdfBrushes.DarkRed);
+                    result = footer.Draw(page, new PointF(0, 0));
+
+                    PdfTextElement s2title = new PdfTextElement($"Printer for  {StoreName}/ Date: {DateTime.Now}", font, PdfBrushes.DarkRed);
+                    result = s2title.Draw(page, new PointF(0, 0));
+
+                 
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    //Saving the PDF document into the stream.
+                    pdfDocument.Save(stream);
+                    //Closing the PDF document.
+                    pdfDocument.Close(true);
+                    return stream;
+                }
+
+            }
+
+        }
+        private MemoryStream CreatePDF(int mon, int year)
+        {
+            //Create a new PDF document.
+            using (PdfDocument pdfDocument = new PdfDocument())
+            {
+                int paragraphAfterSpacing = 8;
+                int cellMargin = 8;
+                //Add page to the PDF document.
+                PdfPage page = pdfDocument.Pages.Add();
+                //Create a new font.
+                PdfStandardFont font = new PdfStandardFont(PdfFontFamily.TimesRoman, 16);
+                //Create a text element to draw a text in PDF page.
+                PdfTextElement title = new PdfTextElement($"Sale Report For Month: {mon}/{year}", font, PdfBrushes.DarkRed);
+
+                PdfLayoutResult result = title.Draw(page, new PointF(0, 0));
+                PdfStandardFont contentFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 12);
+                PdfTextElement content = new PdfTextElement("Sale Report Generated based on Sale.\n\t Report is generated for Each Store", contentFont, PdfBrushes.Black);
+                PdfLayoutFormat format = new PdfLayoutFormat();
+                format.Layout = PdfLayoutType.Paginate;
+                //Draw a text to the PDF document.
+                result = content.Draw(page, new RectangleF(0, result.Bounds.Bottom + paragraphAfterSpacing, page.GetClientSize().Width, page.GetClientSize().Height), format);
+
+                //Create a PdfGrid.
+                PdfGrid pdfGrid = new PdfGrid();
+                pdfGrid.Style.CellPadding.Left = cellMargin;
+                pdfGrid.Style.CellPadding.Right = cellMargin;
+
+                //Assign data source.
+                // pdfGrid.DataSource = forecasts
+
+                //Applying built-in style to the PDF grid.
+                pdfGrid.ApplyBuiltinStyle(PdfGridBuiltinStyle.GridTable4Accent1);
+                pdfGrid.Style.Font = contentFont;
+                //Draw PDF grid into the PDF page.
+                pdfGrid.Draw(page, new Syncfusion.Drawing.PointF(0, result.Bounds.Bottom + paragraphAfterSpacing));
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    //Saving the PDF document into the stream.
+                    pdfDocument.Save(stream);
+                    //Closing the PDF document.
+                    pdfDocument.Close(true);
+                    return stream;
+                }
+
+            }
 
         }
     }
